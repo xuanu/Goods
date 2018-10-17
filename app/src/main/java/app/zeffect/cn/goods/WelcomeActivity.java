3bc96de900da0ece9e8c9407acc25aafeb3a410b
xuanu <625417632@qq.com>
2018-10-17 17:13:29 +0800

    搜索界面

--- app/src/main/java/app/zeffect/cn/goods/WelcomeActivity.java
+++ app/src/main/java/app/zeffect/cn/goods/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package app.zeffect.cn.goods;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import app.zeffect.cn.goods.main.MainActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
