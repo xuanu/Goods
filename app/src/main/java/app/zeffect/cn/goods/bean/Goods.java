2e0a9ff65f9bba7b18a2db071bc969223d4d6487
xuanu <625417632@qq.com>
2018-10-17 18:31:43 +0800

    商品列表

--- app/src/main/java/app/zeffect/cn/goods/bean/Goods.java
+++ app/src/main/java/app/zeffect/cn/goods/bean/Goods.java
@@ -8,4 +8,8 @@ public class Goods {
     private String goodsName;
     private double goodsPrice;
     private String goodsDescribe;//商品描述
+    private int goodsTotal;//商品总数，进货多少商品
+    private int goodsCount;//库存
+    private double goodsPrimeCost;//进货价格，成本
+    private long creatTime;
 }
