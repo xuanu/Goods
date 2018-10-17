2653c524a2d16a46aa2ee02992ddbadf9d5bf0d4
xuanu <625417632@qq.com>
2018-10-17 16:22:16 +0800

    Initial commit

---
+++ app/src/main/java/app/zeffect/cn/goods/bean/Goods.java
@@ -0,0 +1,11 @@
+package app.zeffect.cn.goods.bean;
+
+/***
+ * 商品属性
+ */
+public class Goods {
+    private int id;
+    private String goodsName;
+    private double goodsPrice;
+    private String goodsDescribe;//商品描述
+}
