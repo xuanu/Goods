b7bcf09c329d300b80dfaf788f28e90723cec724
xuanu <625417632@qq.com>
2018-10-19 16:52:21 +0800

    开源许可

--- app/src/main/java/app/zeffect/cn/goods/ui/main/MainActivity.java
+++ app/src/main/java/app/zeffect/cn/goods/ui/main/MainActivity.java
@@ -2,18 +2,29 @@ package app.zeffect.cn.goods.ui.main;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 
 import app.zeffect.cn.goods.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
 
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+            drawerLayout.closeDrawer(Gravity.LEFT);
+            return;
+        }
+        super.onBackPressed();
+    }
 }
