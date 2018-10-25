2af8c58988c04136fffa2588da998c2bdfd7f541
xuanu <625417632@qq.com>
2018-10-25 15:40:10 +0800

    商品首拼

--- app/src/main/java/app/zeffect/cn/goods/utils/GoodsUtils.java
+++ app/src/main/java/app/zeffect/cn/goods/utils/GoodsUtils.java
@@ -6,10 +6,20 @@ import android.text.SpannableStringBuilder;
 import android.text.style.CharacterStyle;
 import android.text.style.ForegroundColorSpan;
 
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -35,4 +45,46 @@ public class GoodsUtils {
         return in.readObject();
     }
 
+    public static List<StringBuilder> getFirstSpell(String word) throws Exception {
+        List<StringBuilder> buffers = new LinkedList<>();
+        buffers.add(new StringBuilder());
+        char[] arr = word.toCharArray();
+        HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
+        defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 128) {
+                String[] temp = PinyinHelper.toHanyuPinyinStringArray(arr[i], defaultFormat);
+                Set<Character> tmpStr = new HashSet(temp.length);
+                if (temp != null) {
+                    for (int j = 0; j < temp.length; j++) {
+                        tmpStr.add(temp[j].charAt(0));
+                    }
+                }
+                if (tmpStr.size() == 1) {
+                    addBuffer(buffers, temp[0].charAt(0));// 都一样，加哪个都行
+                } else if (tmpStr.size() >= 2) {
+                    List<StringBuilder> laStringBuilders = (List<StringBuilder>) cloneObject(buffers);
+                    buffers.clear();
+                    for (Character tmpChar : tmpStr) {// 给以前每一个都加上值
+                        List<StringBuilder> tmpBuilder = (List<StringBuilder>) cloneObject(laStringBuilders);
+                        addBuffer(tmpBuilder, tmpChar);
+                        buffers.addAll(tmpBuilder);
+                        tmpBuilder.clear();
+                    }
+                }
+            } else {
+                addBuffer(buffers, arr[i]);
+            }
+        }
+        //
+        return buffers;
+    }
+
+
+    private static void addBuffer(List<StringBuilder> buffers, char tmpChar) {
+        for (int i = 0; i < buffers.size(); i++) {
+            buffers.get(i).append(tmpChar);
+        }
+    }
 }
